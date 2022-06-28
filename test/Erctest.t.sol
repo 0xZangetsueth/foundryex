e1c31e47730cc6dc7b5d03ac3af6534048274c43
0xZangetsueth <moraleswill@protonmail.com>
2022-06-28 13:48:14 +0400

    initial commit

---
+++ test/Erctest.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/ERC721.sol";
+        
+contract ERC721Test is Test {
+	ERC721 erc721;
+	address bob = address(0x1);
+	address mary = address(0x2);
+
+	function testMintToken() public {
+		erc721 = new ERC721();
+		erc721.mint(bob, 0);
+		address owner_of = erc721.ownerOf(0);
+		assertEq(bob, owner_of);
+	}
+}
